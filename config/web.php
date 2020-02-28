0e5d52c4fbd67aa08df9fd44c93a05e98806c91e
BarakaMgori Marwa <Barakamgori@gmail.com>
2020-02-28 12:34:27 +0300

    adding theme

--- config/web.php
+++ config/web.php
@@ -50,13 +50,7 @@ $config = [
             'rules' => [
             ],
         ],
-        'view' => [
-            'theme' => [
-                'pathMap' => [
-                   '@app/views' => '@vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app'
-                ],
-            ],
-       ],
+    
         
     ],
     'params' => $params,
